e9cb21b75f31b4abe029907ab40dd69ad24aee75
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-01-20 23:25:45 -0500

    Added post new, show actions ind views

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -12,4 +12,32 @@ describe 'navigate' do
       expect(page).to have_content(/Posts/)
     end
   end
+
+  describe 'creation' do
+      before do
+        user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+        login_as(user, :scope => :user)
+        visit new_post_path
+      end
+
+      it 'has a new form that can be reached' do
+        expect(page.status_code).to eq(200)
+      end
+
+      it 'can be created from new form page' do
+        fill_in 'post[date]', with: Date.today
+        fill_in 'post[details]', with: "Some details"
+        click_on "Save"
+
+        expect(page).to have_content("Some details")
+      end
+
+      it 'will have a user associated it' do
+            fill_in 'post[date]', with: Date.today
+            fill_in 'post[details]', with: "User Association"
+            click_on "Save"
+
+            expect(User.last.posts.last.details).to eq("User Association")
+          end
+    end
 end
