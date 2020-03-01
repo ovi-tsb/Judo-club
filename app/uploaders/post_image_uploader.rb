13ab20f85999f0a80fb808d5a76c504e1571cbc7
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-03-01 13:28:43 -0500

    Adde settings for carrierwave for heroku and AWS

--- app/uploaders/post_image_uploader.rb
+++ app/uploaders/post_image_uploader.rb
@@ -3,12 +3,24 @@
 class PostImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+  # storage :file
   # storage :fog
+
+  # You can find a full list of custom headers in AWS SDK documentation on
+    # AWS::S3::S3Object
+    # def download_url(filename)
+    #   url(response_content_disposition: %Q{attachment; filename="#{filename}"})
+    # end
+  
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
