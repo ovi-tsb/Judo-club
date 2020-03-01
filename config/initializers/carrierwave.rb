5c8c103a3dc2cd7247be9c377b85ca0aeb9b0309
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-03-01 18:50:13 -0500

    change Env in config initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ if Rails.env.production?
     # config.fog_provider = 'fog/aws'                        # required
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      AWS_ACCESS_KEY_ID:     ENV["S3_KEY"],        # required
-      AWS_SECRET_ACCESS_KEY: ENV["S3_SECRET"],        # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY_ID"],        # required
+      aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],        # required
       region:                ENV['S3_REGION'],
     }
     config.fog_directory  = ENV["S3_BUCKET"]              # required
