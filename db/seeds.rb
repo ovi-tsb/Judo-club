8b7c9f2b7bfbcf37fb4eff5ae275dd809e1f3bc6
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-03-01 13:08:45 -0500

    Added admin user in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 
 # puts "20 Posts have been created"
 
+@user1 = AdminUser.create!(email: "admin@yahoo.com", 
+                password: "123456", 
+                password_confirmation: "123456", 
+                first_name: "Admin", 
+                last_name: "Admin"
+                )
+
+puts "1 Admin User created"
+
