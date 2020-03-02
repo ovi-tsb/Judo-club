15c30ab411abfb5615e9f9501ccaf87be55314cb
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-03-01 19:24:54 -0500

    Fixing will paginate conflict with administrate

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,16 @@
 
 # puts "20 Posts have been created"
 
+@user1 = User.create!(email: "8@yahoo.com", 
+                password: "123456", 
+                password_confirmation: "123456", 
+                first_name: "User", 
+                last_name: "Regular"
+                )
+
+puts "1 User created"
+
+
 @user1 = AdminUser.create!(email: "admin@yahoo.com", 
                 password: "123456", 
                 password_confirmation: "123456", 
