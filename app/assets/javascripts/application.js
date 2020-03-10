0ddff708b79581f9c3256ac6d5c7745a9e0a4bdc
Ovidiu Turcu <ovi_tsb@yahoo.com>
2020-03-09 22:29:21 -0400

    Fixing paralax on mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -165,29 +165,6 @@ $(document).ready(function () {
 });
 
 
-var isMobile = { 
-    Android: function() { return navigator.userAgent.match(/Android/i); }, 
-    BlackBerry: function() { return navigator.userAgent.match(/BlackBerry/i); }, 
-    iOS: function() { return navigator.userAgent.match(/iPhone|iPad|iPod/i); }, 
-    Opera: function() { return navigator.userAgent.match(/Opera Mini/i); }, 
-    Windows: function() { return navigator.userAgent.match(/IEMobile/i); }, 
-    any: function() { return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows()); } };
 
-    jQuery(function($) {
-        if (isMobile.any()) {
-            document.documentElement.className = document.documentElement.className + " touch";
-
-            $('.parallax-window').each(function(i, obj) {
-
-                $(this).css("background-image", 'url('+$(this).data('image-src')+')');
-                $(this).css("background-color", "#FFFFFF");
-                $(this).css("background-size", "cover");
-                $(this).css("background-position", "center center");
-
-
-            });
-        }
-
-    });
 
 
